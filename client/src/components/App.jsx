710b72be519a1382c8bf1f6e908697bfc201fe9c
Vinh Tran <vincen7.tran@gmail.com>
2019-05-09 16:40:58 -0700

    Gravity Functional

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -4,24 +4,45 @@ import Row from './Row.jsx';
 class App extends React.Component {
   state = {
     playerOneTurn: true,
-    board: {
-      '50': '', '51': '', '52': '', '53': '', '54': '', '55': '', '56': '',
-      '40': '', '41': '', '42': '', '43': '', '34': '', '45': '', '46': '',
-      '30': '', '31': '', '32': '', '33': '', '34': '', '35': '', '36': '',
-      '20': '', '21': '', '22': '', '23': '', '24': '', '25': '', '26': '',
-      '10': '', '11': '', '12': '', '13': '', '14': '', '15': '', '16': '',
-      '00': '', '01': '', '02': '', '03': '', '04': '', '05': '', '06': '',
-    }
+    '60': '', '61': '', '62': '', '63': '', '64': '', '65': '', '66': '',
+    '50': '', '51': '', '52': '', '53': '', '54': '', '55': '', '56': '',
+    '40': '', '41': '', '42': '', '43': '', '44': '', '45': '', '46': '',
+    '30': '', '31': '', '32': '', '33': '', '34': '', '35': '', '36': '',
+    '20': '', '21': '', '22': '', '23': '', '24': '', '25': '', '26': '',
+    '10': '', '11': '', '12': '', '13': '', '14': '', '15': '', '16': '',
   }
 
   handleClick = (e) => {
-    console.log(e.target.id);
+    let id = e.target.id;
+    let square = this.gravity(id);
+    this.markSpace(square);
+  }
+
+  gravity = (id) => {
+    let numID = parseInt(id);
+    
+    while (numID > numID % 10 + 10) {
+      if (this.state[numID - 10] !== '') break;
+      numID = numID - 10;
+    }
+    let square = document.getElementById(`${numID}`);
+
+    return square;
+  }
+
+  markSpace = (square) => {
+    if (this.state.playerOneTurn) {
+      square.style.backgroundColor = 'red';
+    } else {
+      square.style.backgroundColor = 'black' ;
+    }
+    this.setState({[square.id]: this.state.playerOneTurn, playerOneTurn: !this.state.playerOneTurn});
   }
 
   render() {
    const renderedRows = [];
    
-   for (let i = 5; i >= 0; i--) {
+   for (let i = 6; i >= 1; i--) {
      renderedRows.push(<Row key={i} y={i} handleClick={this.handleClick} />);
    }
 
